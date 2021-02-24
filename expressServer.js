392b4b8ded0c22f4594c14a7547a4ef7e5dbae99
goo2821 <milk5352@gmail.com>
2021-02-24 11:11:29 +0900

    디자인 파일 추가

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mysql = require('mysql');
-const app = express()
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', __dirname + '/views');//디렉토리 설정
 app.set('view engine', 'ejs');//뷰 엔진 ejs 설정
@@ -21,6 +26,13 @@ app.get('/', function (req, res) {
 app.get('/ejs', function(req, res){
   res.render('ejsTest');
 })
+
+app.post('/userData', function(req, res){
+  console.log("사용자의 요청이 전송되었습니다. 들어온 데이터는 " + req.body.userData1 + " " + req.body.userData2);
+  connection.query('SELECT * FROM user', function(err, results, fields) {
+    res.send(results);
+  })
+});
  
 app.get('/user', function(req, res) {
   connection.query('SELECT * FROM fintech.user', function (error, results, fields) {
