a14809620317745f1ccda832b0e9afca5bce3a4f
goo2821 <milk5352@gmail.com>
2021-02-26 17:13:57 +0900

    completion

--- expressServer.js
+++ expressServer.js
@@ -32,7 +32,6 @@ app.get('/ejs', function (req, res) {
 })
 
 app.post('/userData', function (req, res) {
-  console.log("사용자의 요청이 전송되었습니다. 들어온 데이터는 " + req.body.userData1 + " " + req.body.userData2);
   connection.query('SELECT * FROM user', function (err, results, fields) {
     res.send(results);
   })
@@ -82,8 +81,6 @@ app.post('/signup', function (req, res) {
   var userRefreshToken = req.body.userRefreshToken;
   var userSeqNo = req.body.userSeqNo;
 
-  console.log(userName + " " + userEmail + " " + userPassword + " " + userAccessToken + " " + userRefreshToken + " " + userSeqNo);
-
   var sql = "INSERT INTO fintech.user (name, email, password, accesstoken, refreshtoken, userseqno) VALUES (?, ?, ?, ?, ?, ?)";
 
   connection.query(sql, [userName, userEmail, userPassword, userAccessToken, userRefreshToken, userSeqNo], function (err, result) {
@@ -103,16 +100,13 @@ app.get('/login', function (req, res) {
 app.post('/login', function (req, res) {
   var userEmail = req.body.userEmail;
   var userPassword = req.body.userPassword;
-  console.log(userEmail, userPassword)
   var sql = "SELECT * FROM user WHERE email = ?";
   connection.query(sql, [userEmail], function (err, result) {
     if (err) {
-      console.error(err);
       res.json(0);
       throw err;
     }
     else {
-      console.log(result);
       if (result.length == 0) {
         res.json(3)
       }
@@ -132,7 +126,6 @@ app.post('/login', function (req, res) {
               subject: 'user.login.info'
             },
             function (err, token) {
-              console.log('로그인 성공', token)
               res.json(token)
             }
           )
@@ -145,20 +138,19 @@ app.post('/login', function (req, res) {
   })
 })
 
-app.get('/authTest', auth, function(req, res){
+app.get('/authTest', auth, function (req, res) {
   res.send("정상적으로 로그인 하셨다면 해당 화면이 보입니다.");
 })
 
-app.get('/main', function(req, res) {
+app.get('/main', function (req, res) {
   res.render('main');
 })
 
-app.post('/list', auth, function(req, res){
+app.post('/list', auth, function (req, res) {
   var user = req.decoded;
-  
   var sql = "SELECT * FROM user WHERE id = ?";
-  connection.query(sql, [user.userId], function(err, result){
-    if(err) throw err;
+  connection.query(sql, [user.userId], function (err, result) {
+    if (err) throw err;
     else {
       var dbUserData = result[0];
       var option = {
@@ -171,10 +163,9 @@ app.post('/list', auth, function(req, res){
           user_seq_no: dbUserData.userseqno,
         }
       }
-    
+
       request(option, function (err, response, body) {
         if (err) {
-          console.log(err);
           throw err;
         } else {
           var result = JSON.parse(body);
@@ -185,16 +176,16 @@ app.post('/list', auth, function(req, res){
   })
 })
 
-app.get('/balance', function(req, res) {
+app.get('/balance', function (req, res) {
   res.render('balance');
 })
 
-app.post('/balance', auth, function(req, res) {
+app.post('/balance', auth, function (req, res) {
   var user = req.decoded;
-  
+
   var sql = "SELECT * FROM user WHERE id = ?";
-  connection.query(sql, [user.userId], function(err, result){
-    if(err) throw err;
+  connection.query(sql, [user.userId], function (err, result) {
+    if (err) throw err;
     else {
       var dbUserData = result[0];
       var finusernum = req.body.fin_use_num;
@@ -227,34 +218,92 @@ app.post('/balance', auth, function(req, res) {
   })
 })
 
-app.post('/transactionList', auth, function(req, res){
+app.post('/transactionList', auth, function (req, res) {
   var user = req.decoded;
   var finusernum = req.body.fin_use_num;
   var countnum = Math.floor(Math.random() * 1000000000) + 1;
   var transId = "M202111575U" + countnum;
   var transdtime = moment(new Date()).format('YYYYMMDDhhmmss');
-  console.log(transdtime);
   var sql = "SELECT * FROM user WHERE id = ?";
+  connection.query(sql, [user.userId], function (err, result) {
+    if (err) throw err;
+    else {
+      var dbUserData = result[0];
+      var option = {
+        method: "GET",
+        url: "https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num",
+        headers: {
+          Authorization: "Bearer " + dbUserData.accesstoken
+        },
+        qs: {
+          bank_tran_id: transId,
+          fintech_use_num: finusernum,
+          inquiry_type: 'A',
+          inquiry_base: 'D',
+          from_date: '20190101',
+          to_date: '20190101',
+          sort_order: 'D',
+          tran_dtime: transdtime
+        }
+      }
+      request(option, function (err, response, body) {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+        else {
+          var transactionListResuult = JSON.parse(body);
+          res.json(transactionListResuult)
+        }
+      })
+    }
+  })
+})
+
+app.get('/qrcode', function (req, res) {
+  res.render('qrcode');
+})
+
+app.get('/qrreader', function (req, res) {
+  res.render('qrreader');
+})
+
+app.post('/withdraw', auth, function(req, res){
+  //사용자 출금이체 API 수행하기
+  console.log(req.body);
+  var user = req.decoded;
+  var sql = "SELECT * FROM user WHERE id = ?";
+  var countnum = Math.floor(Math.random() * 1000000000) + 1;
+  var transId = companyId + countnum;  
+  var transdtime = moment(new Date()).format('YYYYMMDDhhmmss');
+
   connection.query(sql,[user.userId], function(err, result){
       if(err) throw err;
       else {
           var dbUserData = result[0];
           console.log(dbUserData);
           var option = {
-              method : "GET",
-              url : "https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num",
+              method : "POST",
+              url : "https://testapi.openbanking.or.kr/v2.0/transfer/withdraw/fin_num",
               headers : {
                   Authorization : "Bearer " + dbUserData.accesstoken
               },
-              qs : {
-                  bank_tran_id : transId,
-                  fintech_use_num : finusernum,
-                  inquiry_type : 'A',
-                  inquiry_base : 'D',
-                  from_date : '20190101',
-                  to_date : '20190101',
-                  sort_order : 'D',
-                  tran_dtime : transdtime
+              json : {
+                  "bank_tran_id" : transId,
+                  "cntr_account_type" : "N",
+                  "cntr_account_num" : "200000000001",
+                  "dps_print_content": "쇼핑몰환불",
+                  "fintech_use_num": req.body.fin_use_num,
+                  "wd_print_content": "오픈뱅킹출금",
+                  "tran_amt": req.body.amount,
+                  "tran_dtime": transdtime,
+                  "req_client_name": "홍길동",
+                  "req_client_fintech_use_num" : req.body.fin_use_num,
+                  "req_client_num": "HONGGILDONG1234",
+                  "transfer_purpose": "ST",
+                  "recv_client_name": "홍길동",
+                  "recv_client_bank_code": "097",
+                  "recv_client_account_num": "100000000001"
               }
           }
           request(option, function(err, response, body){
@@ -263,17 +312,54 @@ app.post('/transactionList', auth, function(req, res){
                   throw err;
               }
               else {
-                  var transactionListResuult = JSON.parse(body);
-                  res.json(transactionListResuult)
+                  var transactionListResuult = body;
+                  if(transactionListResuult.rsp_code === "A0000"){
+                      var countnum2 = Math.floor(Math.random() * 1000000000) + 1;
+                      var transId2 = companyId + countnum2;  
+                      var transdtime2 = moment(new Date()).format('YYYYMMDDhhmmss');                    
+                      var option = {
+                          method : "POST",
+                          url : "https://testapi.openbanking.or.kr/v2.0/transfer/deposit/fin_num",
+                          headers : {
+                            Authorization : "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJUOTkxNTk5MTkwIiwic2NvcGUiOlsib29iIl0sImlzcyI6Imh0dHBzOi8vd3d3Lm9wZW5iYW5raW5nLm9yLmtyIiwiZXhwIjoxNjIyMDg5NjAzLCJqdGkiOiIxOTFiNDE4MS1kN2UzLTRlMTMtYmM3My1hOThmODE5NmJlMjcifQ.bqhF4uaAIqtNVtzyadVOtat0WwPQgWYHpoecEaCz9yY"
+                          },
+                          //get 요청을 보낼때 데이터는 qs, post 에 form, json 입력가능
+                          json : {
+                            "cntr_account_type": "N",
+                            "cntr_account_num": "200000000001",
+                            "wd_pass_phrase": "NONE",
+                            "wd_print_content": "환불금액",
+                            "name_check_option": "off",
+                            "tran_dtime": transdtime2,
+                            "req_cnt": "1",
+                            "req_list": [
+                              {
+                                "tran_no": "1",
+                                "bank_tran_id": transId2,
+                                "fintech_use_num": req.body.to_fin_use_num,
+                                "print_content": "쇼핑몰환불",
+                                "tran_amt": req.body.amount,
+                                "req_client_name": "홍길동",
+                                "req_client_num": "HONGGILDONG1234",
+                                "req_client_fintech_use_num": req.body.fin_use_num,
+                                "transfer_purpose": "ST"
+                              }
+                            ]
+                          }
+                      }
+                      request(option, function (error, response, body) {
+                          console.log(body);
+                          res.json(body);
+                      });
+              
+                  }
+                  //입금 api 실행 A0000 res_code 입급이체 발생
               }
           })        
       }
   })
 })
 
-app.get('/qrcode', function(req, res){
-  res.render('qrcode');
-})
 
 app.get('/user', function (req, res) {
   connection.query('SELECT * FROM fintech.user', function (error, results, fields) {
